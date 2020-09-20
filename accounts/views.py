c8c0b7fc629246adafe20834e9b0e10596c70efc
jaredliw <jaredliw@gmail.com>
2020-09-20 22:45:59 +0800

    Create new application -- accounts

---
+++ accounts/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+def register(request):
+    return render(request, 'accounts/register.html')
--- web_design/settings.py
+++ web_design/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'home.apps.HomeConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
