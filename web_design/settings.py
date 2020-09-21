93ab83b32ad19d141ba2e3d1ca80401a523a18f3
jaredliw <jaredliw@gmail.com>
2020-09-21 21:11:19 +0800

    Create new application -- curriculum

--- web_design/settings.py
+++ web_design/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'home.apps.HomeConfig',
     'accounts.apps.AccountsConfig',
+    'curriculum.apps.CurriculumConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
