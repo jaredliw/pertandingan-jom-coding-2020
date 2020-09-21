a2b3e9625b2a51634c39d7ae5735e88218da73a0
jaredliw <jaredliw@gmail.com>
2020-09-21 21:11:47 +0800

    Create new application -- curriculum

--- curriculum/models.py
+++ curriculum/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
