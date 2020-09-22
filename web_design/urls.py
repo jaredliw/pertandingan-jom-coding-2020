3fe51613c74a70f298b8d2671fb8b5136ac8f9b6
jaredliw <jaredliw@gmail.com>
2020-09-22 20:43:33 +0800

    Route to new app -- curriculum

--- web_design/urls.py
+++ web_design/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
+    path('curriculum/', include('curriculum.urls'))
 ]
