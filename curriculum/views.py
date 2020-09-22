f62021f76d1d4a49798a00e78543cfd69b2ec612
jaredliw <jaredliw@gmail.com>
2020-09-22 20:44:15 +0800

    Complete curriculum.html (UI only)

--- curriculum/views.py
+++ curriculum/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def curriculum(request):
+    kwargs = {
+        "nav_styling": "background-color: #FFF;",
+        "nav_text_styling": "color: #000;"
+    }
+    return render(request, 'curriculum/curriculum.html', kwargs)
