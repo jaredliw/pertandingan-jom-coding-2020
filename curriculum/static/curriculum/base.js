3edc6f05a5b7f60fd0125958b9c86c6c6c1bd94d
jaredliw <jaredliw@gmail.com>
2020-09-24 12:26:30 +0800

    Final bug and typo fix -- home

--- curriculum/static/curriculum/base.js
+++
@@ -1,6 +0,0 @@
-$("#one-line-js").keypress(function(event) {
-    if (event.which == 13) {
-        alert("Function is Called on Enter");
-        event.preventDefault(); //Add this line to your code
-    }
-});
