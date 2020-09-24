4eea2cf4d861c6d67e3ab576d2a6f238edb45069
jaredliw <jaredliw@gmail.com>
2020-09-24 13:25:10 +0800

    Final bug and typo fix -- curriculum

--- curriculum/models.py
+++ curriculum/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 
 
 class Quote(models.Model):
-    author = models.CharField(default="", max_length=25)
-    quote = models.TextField(default="", max_length=100)
+    author = models.CharField(default="Anonim", max_length=25, blank=False)
+    quote = models.TextField(max_length=100, blank=False)
 
 class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField()
+    username = models.CharField(default="Anonim", max_length=30, blank=False)
+    content = models.TextField(blank=False)
     date = models.DateTimeField(auto_now_add=True)
--- curriculum/views.py
+++ curriculum/views.py
@@ -1,36 +1,57 @@
-from django.shortcuts import render
-from .models import Quote
+from django.shortcuts import render, redirect
+from django.urls import resolve
+from .models import Quote, Comment
 from random import choice
+from urllib import parse
 
-
-quotes = Quote.objects.all()
-quote = choice(quotes)
 kwargs = {
     "nav_styling": "background-color: #FFF;",
     "nav_text_styling": "color: #000;",
-    "author": "Jayden Teh Jing Siang",
-    "quote": quote.quote,
-    "quote_author": quote.author
+    "author": "Jayden Teh Jing Siang"
 }
 
+def preprocess(func):
+    def process(request):
+        quotes = Quote.objects.all()
+        quote = choice(quotes)
+        kwargs["quote"] = quote.quote
+        kwargs["quote_author"] = quote.author
+
+        comments = Comment.objects.all()
+        kwargs["comments"] = comments
+
+        if request.method == "POST":
+            data = dict(parse.parse_qsl(request.body))
+            Comment(username=data[b'username'].decode(), content=data[b'comment'].decode()).save()
+            # Get name by path, refer to .urls.py for more infomation
+            path_name = resolve(request.path_info).url_name
+            # Redirect POST to GET, preventing reeated POST on reload
+            return redirect(path_name)
+        return func(request)
+    return process
+
+@preprocess
 def comp_t(request):
     kwargs["title"] = "Teknik Pemikiran Komputasional"
     kwargs["cover_img"] = "images/1.jpg"
     kwargs["subtitile"] = "to be added..."
     return render(request, 'curriculum/comp_t.html', kwargs)
 
+@preprocess
 def data_r(request):
     kwargs["title"] = "Perwakilan Data"
     kwargs["cover_img"] = "images/2.jpg"
     kwargs["subtitile"] = "to be added..."
     return render(request, 'curriculum/data_r.html', kwargs)
 
+@preprocess
 def algo(request):
     kwargs["title"] = "Algorithma"
     kwargs["cover_img"] = "images/3.jpg"
     kwargs["subtitile"] = "to be added..."
     return render(request, 'curriculum/algo.html', kwargs)
 
+@preprocess
 def code(request):
     kwargs["title"] = "Kod Arahan"
     kwargs["cover_img"] = "images/3.jpg"
