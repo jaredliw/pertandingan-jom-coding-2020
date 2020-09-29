5f673bfc04a7fc220c3aeeafe5e7a4c3e2a43622
jaredliw <jaredliw@gmail.com>
2020-09-29 13:22:50 +0800

    Bug fix for quiz page

--- quiz/views.py
+++ quiz/views.py
@@ -13,16 +13,16 @@ default_kwargs = {
 
 kwargs = {}
 
-def get_qn(path_name, qn_seed):
+def get_qn(path_name, qn_seed, n=1):
     # Set seed
     seed(qn_seed)
     # Pick a random question
     qns = Question.objects.filter(page=path_name)
-    qn = choice(qns)
+    for _ in range(n):
+        qn = choice(qns)
     return qn
 
 def shuffle_opt(qn, opt_seed):
-    print(type(opt_seed))
     # Set seed
     seed(opt_seed)
     # shuffle options
@@ -39,70 +39,77 @@ def preprocess(func):
             # Get data in dict
             data = dict(parse.parse_qsl(request.body))
 
-            # Get question that is anwering now
-            qn = Question.objects.filter(qn_id=data[b'qn_id'].decode(), page=path_name).first()
-            # Get answer in text
-            qn_ans = qn.__getattribute__(qn.answer)
-            # Get index of answer after shuffling
-            qn_ans_idx = shuffle_opt(qn, int(data[b'opt_seed'].decode())).index(qn_ans)
+            if int(data[b'load_next']):
+                kwargs.clear()
+                kwargs["page"] = int(data[b'page']) + 1
+                qn = get_qn(path_name, int(data[b'qn_seed']), kwargs["page"])
+                kwargs["question"] = qn
 
-            # Get index of user's answer
-            mapping = ["opt1", "opt2", "opt3", "opt4"]
-            user_ans_idx = mapping.index(data[b'user_opt'].decode())
-
-            # Show message
-            if qn_ans_idx == user_ans_idx:
-                messages.success(request, "Tahniah! Jawapan anda adalah betul!")
+                # Get a random seed (for opt)
+                opt_seed = randint(1, 100000)
+                kwargs['opt_seed'] = opt_seed
+                opt_l = shuffle_opt(qn, opt_seed)
+                qn.opt1, qn.opt2, qn.opt3, qn.opt4 = opt_l
+                kwargs["qn_seed"] = int(data[b'qn_seed'])
             else:
-                hint_ans = ["A", "B", "C", "D"]
-                messages.error(request, "Maaf, jawpan betul ialah {}.".format(hint_ans[qn_ans_idx]))
+                # Get question that is anwering now
+                qn = Question.objects.filter(qn_id=data[b'qn_id'].decode(), page=path_name).first()
+                # Get answer in text
+                qn_ans = qn.__getattribute__(qn.answer)
+                # Get index of answer after shuffling
+                qn_ans_idx = shuffle_opt(qn, int(data[b'opt_seed'])).index(qn_ans)
 
-            # Disable buttons
-            kwargs['{}_selected'.format(data[b'user_opt'].decode())] = 'checked'
-            kwargs['status'] = 'disabled'
-            # kwargs["page_now"] += 1
-            return render(request, 'quiz/base.html', kwargs)
+                # Get index of user's answer
+                mapping = ["opt1", "opt2", "opt3", "opt4"]
+                user_ans_idx = mapping.index(data[b'user_opt'].decode())
 
-        kwargs.clear()
-        # Get a random seed (for qn)
-        qn_seed = randint(1, 100000)
-        kwargs['qn_seed'] = qn_seed
-        qn = get_qn(path_name, qn_seed)
-        kwargs["question"] = qn
+                # Show message
+                if qn_ans_idx == user_ans_idx:
+                    messages.success(request, "Tahniah! Jawapan anda adalah betul!")
+                else:
+                    hint_ans = ["A", "B", "C", "D"]
+                    messages.error(request, "Maaf, jawpan betul ialah {}.".format(hint_ans[qn_ans_idx]))
 
-        # Get a random seed (for opt)
-        opt_seed = randint(1, 100000)
-        kwargs['opt_seed'] = opt_seed
-        opt_l = shuffle_opt(qn, opt_seed)
-        qn.opt1, qn.opt2, qn.opt3, qn.opt4 = opt_l
+                # Disable buttons
+                kwargs['{}_selected'.format(data[b'user_opt'].decode())] = 'checked'
+                kwargs['status'] = 'disabled'
+            kwargs["page_total"] = Question.objects.filter(page=path_name).count()
+            return render(request, 'quiz/base.html', {**kwargs, **default_kwargs})
+        else:
+            kwargs.clear()
+            # Get a random seed (for qn)
+            qn_seed = randint(1, 100000)
+            kwargs['qn_seed'] = qn_seed
+            qn = get_qn(path_name, qn_seed)
+            kwargs["question"] = qn
 
-        kwargs["page_now"] = 1
-        kwargs["page_total"] = Question.objects.filter(page=path_name).count()
-        return func(request)
+            # Get a random seed (for opt)
+            opt_seed = randint(1, 100000)
+            kwargs['opt_seed'] = opt_seed
+            opt_l = shuffle_opt(qn, opt_seed)
+            qn.opt1, qn.opt2, qn.opt3, qn.opt4 = opt_l
+
+            kwargs["page"] = 1
+            kwargs["page_total"] = Question.objects.filter(page=path_name).count()
+            return func(request)
     return process
 
 @preprocess
 def comp_t(request):
-    default_kwargs["quiz_name"] = "Hallo"
+    default_kwargs["quiz_name"] = "Pemikiran Komputasional"
     return render(request, 'quiz/base.html', {**kwargs, **default_kwargs})
 
 @preprocess
 def data_r(request):
-    kwargs["title"] = "Perwakilan Data"
-    kwargs["cover_img"] = "images/2.jpg"
-    kwargs["subtitile"] = "to be added..."
-    return render(request, 'curriculum/data_r.html', kwargs)
+    default_kwargs["quiz_name"] = "Perwakilan Data"
+    return render(request, 'quiz/base.html', {**kwargs, **default_kwargs})
 
 @preprocess
 def algo(request):
-    kwargs["title"] = "Algorithma"
-    kwargs["cover_img"] = "images/3.jpg"
-    kwargs["subtitile"] = "to be added..."
-    return render(request, 'curriculum/algo.html', kwargs)
+    default_kwargs["quiz_name"] = "Algoritma"
+    return render(request, 'quiz/base.html', {**kwargs, **default_kwargs})
 
 @preprocess
 def code(request):
-    kwargs["title"] = "Kod Arahan"
-    kwargs["cover_img"] = "images/3.jpg"
-    kwargs["subtitile"] = "to be added..."
-    return render(request, 'curriculum/code.html', kwargs)
+    default_kwargs["quiz_name"] = "Kod Arahan"
+    return render(request, 'quiz/base.html', {**kwargs, **default_kwargs})
